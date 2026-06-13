2597fab569e961af60f40d8b849beff1eff7aac7
Sri Hari <sriharir1711@gmail.com>
2026-06-13 19:38:02 +0530

    fix: comment out fileConfig in alembic env to prevent KeyError since alembic.ini has no logging config

--- alembic/env.py
+++ alembic/env.py
@@ -24,7 +24,7 @@ config.set_main_option("sqlalchemy.url", settings.DATABASE_URL)
 # Interpret the config file for Python logging.
 # This line sets up loggers basically.
 if config.config_file_name is not None:
-    fileConfig(config.config_file_name)
+    pass # fileConfig(config.config_file_name)
 
 # add your model's MetaData object here
 # for 'autogenerate' support
